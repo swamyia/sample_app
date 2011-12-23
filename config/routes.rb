049610bfb2880aada76cd83928e1b87e1fc70a5e
subramanian <whisp_official@yahoo.com>
2011-12-23 13:17:36 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 SampleApp::Application.routes.draw do
+
+  
+  get "users/new"	# generated when the controller is generated.
+  match '/signup',	:to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
